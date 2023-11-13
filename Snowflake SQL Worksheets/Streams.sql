dc23f907c2aa59a17955b92dcd7b820ad9c5643f
Krithi Sri <39584852+krithisri@users.noreply.github.com>
2023-11-13 13:37:34 -0500

    Update Streams.sql

--- Snowflake SQL Worksheets/Streams.sql
+++ Snowflake SQL Worksheets/Streams.sql
@@ -2,7 +2,7 @@
 USE DATABASE SNOWFLAKE_BRONZE;
 
 -- Create a stream named SAMPLE_STREAM on the BRONZE table.
-CREATE OR REPLACE STREAM SAMPLE_STREAM ON TABLE SAS.BRONZE;
+CREATE OR REPLACE STREAM SAMPLE_STREAM ON TABLE S3.BRONZE;
 -- Uncomment the following line to show streams.
 -- SHOW STREAMS;
 -- Select all records from the SAMPLE_STREAM stream.
